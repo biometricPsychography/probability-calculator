de69953e8c15e1008e0c06b15548e4dc442f0c6f
biometricPsychography <cyclinglinguist@gmail.com>
2022-11-25 21:48:56 -0600

    Begin

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,21 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    
+    def __init__(self, **balls) :
+        self.contents = []
+        for key in balls :
+            i = 0
+            while i < balls[key] :
+                self.contents.append(key)
+                i += 1
 
+        print(self.contents)
+
+    def draw(self, num_to_draw) :
+        random.randint(0, len(self.contents))
+
+Hat(blue=2, red=5)
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
